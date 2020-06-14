66b75bed14d700df6940e0caca600d0d697f4f08
Dibya Ranjan Mallick <dmallick@salesforce.com>
2020-06-15 03:02:45 +0530

    first draft

---
+++ popup.js
@@ -0,0 +1,35 @@
+let pasteUrls = document.getElementById('pasteUrls');
+let copiedUrls = document.getElementById('copiedUrls');
+let openInTabs = document.getElementById('openInTabs');
+let urls = "";
+
+pasteUrls.onclick = function(element) {
+    copiedUrls.focus();
+    document.execCommand('paste');
+
+    urls = copiedUrls.value.split('\n');
+    var buttonText = openInTabs.innerText;
+    buttonText += "(" + tabs.length + ")";
+    // alert(buttonText);
+    openInTabs.innerText = buttonText;
+};
+
+
+
+function onCreated(tab) {
+    console.log(`Created new tab: ${tab.url}`);
+}
+
+function onError(error) {
+    console.error(`Error: ${error}`);
+}
+
+openInTabs.onclick = function(element) {
+    urls.forEach(url => {
+        var creating = browser.tabs.create({
+           url: url 
+        });
+
+        creating.then(onCreated, onError);
+    });
+}
